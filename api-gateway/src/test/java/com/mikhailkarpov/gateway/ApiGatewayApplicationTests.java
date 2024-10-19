0daf58ccd6c7def25f6989e100bd8fd1c7415d93
mikhail-karpov <mikhailkarpov@hotmail.com>
2024-10-19 20:13:36 +0400

    fix: ApiGatewayApplicationTests

--- api-gateway/src/test/java/com/mikhailkarpov/gateway/ApiGatewayApplicationTests.java
+++ api-gateway/src/test/java/com/mikhailkarpov/gateway/ApiGatewayApplicationTests.java
@@ -1,13 +1,20 @@
 package com.mikhailkarpov.gateway;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 
 @SpringBootTest
 class ApiGatewayApplicationTests {
 
+	@MockBean
+	private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
 	@Test
 	void contextLoads() {
+		Assertions.assertThat(clientRegistrationRepository).isNotNull();
 	}
 
 }
