62480ec57398d024a25c3d8c19da73da1e4f13fd
mikhail-karpov <mikhailkarpov@hotmail.com>
2024-10-19 20:29:57 +0400

    init: frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Spring Security OAuth2 Demo</h1>
+      <div className="card">
+        Work in progress
+      </div>
+    </>
+  )
+}
+
+export default App
